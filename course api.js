0b8f2a5b21fb1544fbf4193ab0359fe1e07abf1c
Md. Rabbinur Muktar <59724876+Rabbinur@users.noreply.github.com>
2023-10-31 17:24:08 +0600

    Create course api.js

---
+++ course api.js
@@ -0,0 +1,382 @@
+export const course = [
+  {
+    id: 1,
+    name: "IELTS Course",
+    classRunning: "3",
+    courseOverview: {
+      durations: 3,
+      classes: 30,
+      extraClasses: 322,
+      mock: 200,
+    },
+    CourseFee: {
+      CourseFee: 25000,
+      BooksFee: 2750,
+      TotalFee: 17750,
+    },
+    onlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "9:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "9:00 PM ",
+      },
+    ],
+    offlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "3:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Level-0 (Beginner)",
+    classRunning: "6",
+    offlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "3:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+    ],
+    onlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "9:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "9:00 PM ",
+      },
+    ],
+    courseOverview: {
+      durations: 2,
+      classes: 30,
+      extraClasses: 322,
+      mock: 200,
+    },
+    CourseFee: {
+      CourseFee: 35000,
+      BooksFee: 2750,
+      TotalFee: 17750,
+    },
+  },
+  {
+    id: 3,
+    offlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "3:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+    ],
+    onlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "9:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "9:00 PM ",
+      },
+    ],
+    name: "FreeLancing",
+    classRunning: "3",
+    courseOverview: {
+      durations: 3,
+      classes: 30,
+      extraClasses: 322,
+      mock: 200,
+    },
+    CourseFee: {
+      CourseFee: 15000,
+      BooksFee: 2750,
+      TotalFee: 17750,
+    },
+  },
+  {
+    id: 3,
+    classRunning: "5",
+    offlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "3:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+    ],
+    onlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "9:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "9:00 PM ",
+      },
+    ],
+    name: "FreeLancing1",
+    courseOverview: {
+      durations: 3,
+      classes: 30,
+      extraClasses: 322,
+      mock: 200,
+    },
+    CourseFee: {
+      CourseFee: 45000,
+      BooksFee: 2750,
+      TotalFee: 17750,
+    },
+  },
+  {
+    id: 4,
+    classRunning: "5",
+    offlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "3:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "10:00 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "11:30 AM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "5:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+    ],
+    onlineSchedule: [
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sat - Mon -Wed",
+        classTime: "9:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "7:00 PM ",
+      },
+      {
+        routine: " Sun - Tue -Thu",
+        classTime: "9:00 PM ",
+      },
+    ],
+    name: "FreeLancing2",
+    courseOverview: {
+      durations: 3,
+      classes: 30,
+      extraClasses: 322,
+      mock: 200,
+    },
+    CourseFee: {
+      CourseFee: 55000,
+      BooksFee: 2750,
+      TotalFee: 17750,
+    },
+  },
+];
